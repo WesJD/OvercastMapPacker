7f8919581afbd64cbf7ecc73948d6adfa2cfd8bd
WesJD <wesjavadev@gmail.com>
2016-06-10 18:35:13 -0400

    Clean up handling in ContributorsInventory

--- src/main/java/com/github/wesjd/overcastmappacker/mc/inventory/ContributorsInventory.java
+++ src/main/java/com/github/wesjd/overcastmappacker/mc/inventory/ContributorsInventory.java
@@ -2,13 +2,16 @@ package com.github.wesjd.overcastmappacker.mc.inventory;
 
 import com.github.wesjd.overcastmappacker.mc.XMLWorld;
 import com.github.wesjd.overcastmappacker.util.*;
+import com.github.wesjd.overcastmappacker.xml.module.ParentXMLModule;
+import com.github.wesjd.overcastmappacker.xml.module.XMLModule;
+import com.github.wesjd.overcastmappacker.xml.module.impl.general.main.AuthorModule;
 import com.github.wesjd.overcastmappacker.xml.module.impl.general.main.ContributorModule;
+import com.github.wesjd.overcastmappacker.xml.module.impl.general.main.parents.AuthorsParentModule;
 import com.github.wesjd.overcastmappacker.xml.module.impl.general.main.parents.ContributorsParentModule;
 import net.buildstatic.util.anvilgui.AnvilGUI;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
 
 import java.util.Arrays;
 import java.util.UUID;
@@ -54,58 +57,68 @@ public class ContributorsInventory extends AbstractEditorInventory {
         set(3, Items.build(ChatColor.GREEN + "Add Contributor", Material.BOOK), new Button() {
             @Override
             public void onClick(Player clicker) {
-                new YesNoInventory(clicker, Items.build("Do they have an account?", Material.GRASS, Arrays.asList("Does this person own a", "Minecraft account?"))) {
-                    @Override
-                    public void onYes(Player decider) {
-                        new InputAnvil(decider, "Their ingame name?", new AnvilGUI.ClickHandler() {
-                            @Override
-                            public String onClick(Player player, String input) {
-                                try {
-                                    final UUID uuid = UUIDFetcher.getUUIDOf(input);
-                                    if(uuid != null) {
-                                        final String uuidString = uuid.toString();
-                                        handleContribution(true, player, new String[] { uuidString, null });
-                                    } else return ChatColor.RED + "Not a valid name.";
-                                } catch (Exception ex) {
-                                    throw new RuntimeException(ex);
-                                }
-                                return input; //no flicker
-                            }
-                        });
-                    }
-
-                    @Override
-                    public void onNo(Player decider) {
-                        decider.sendMessage("you said no");
-                    }
-                };
+                handle(player, true);
             }
         });
 
         set(5, Items.build(ChatColor.GRAY + "Add Author", Material.BOOK), new Button() {
             @Override
             public void onClick(Player clicker) {
-
+                handle(player, false);
             }
         });
 
         //TODO - Show all contributors and make them able to be removed
     }
 
-    private void handleContribution(boolean contributor, Player player, String[] name) {
+    private void handle(Player player, boolean contributor) {
+        new YesNoInventory(player, Items.build("Do they have an account?", Material.GRASS, Arrays.asList("Does this person own a", "Minecraft account?"))) {
+            @Override
+            public void onYes(Player decider) {
+                new InputAnvil(decider, "Their ingame name?", new AnvilGUI.ClickHandler() {
+                    @Override
+                    public String onClick(Player player, String input) {
+                        try {
+                            final UUID uuid = UUIDFetcher.getUUIDOf(input);
+                            if(uuid != null) {
+                                final String uuidString = uuid.toString();
+                                handleAdd(contributor, player, new String[] { uuidString, null });
+                            } else return ChatColor.RED + "Not a valid name.";
+                        } catch (Exception ex) {
+                            throw new RuntimeException(ex);
+                        }
+                        return input; //no flicker
+                    }
+                });
+            }
+
+            @Override
+            public void onNo(Player decider) {
+                new InputAnvil(decider, "What is thier name?", new AnvilGUI.ClickHandler() {
+                    @Override
+                    public String onClick(Player player, String input) {
+                        handleAdd(contributor, decider, new String[] { null, input });
+                        return input;
+                    }
+                });
+            }
+        };
+    }
+
+    private void handleAdd(boolean contributor, Player player, String[] name) {
         //name[0] = uuid, name[1] = name
         new InputAnvil(player, "Their contribution?", new AnvilGUI.ClickHandler() {
             @Override
             public String onClick(Player player, String contribution) {
-                if(contributor) {
-                    if (name[0] != null) ContributorsInventory.super.documentHandler.add(ContributorsParentModule.class, ContributorModule.class,
-                                ContinuingMap.from("uuid", name[0]).add("contribution", contribution));
-                    else ContributorsInventory.super.documentHandler.add(ContributorsParentModule.class, ContributorModule.class, name[1],
-                                ContinuingMap.from("contribution", contribution));
+                final Class<? extends ParentXMLModule> parentModule = (contributor ? ContributorsParentModule.class : AuthorsParentModule.class);
+                final Class<? extends XMLModule> module = (contributor ? ContributorModule.class : AuthorModule.class);
+                final ContinuingMap<String, String> attributes = new ContinuingMap<>();
+                if(name[0] != null) attributes.add("uuid", name[0]);
+                attributes.add("contribution", contribution);
 
-                } else {
-                    //TODO - Handle author
-                }
+                if(name[0] != null) ContributorsInventory.super.documentHandler.add(parentModule, module, attributes);
+                else ContributorsInventory.super.documentHandler.add(parentModule, module, name[1], attributes);
+
                 new ContributorsInventory(player, ContributorsInventory.super.xmlWorld);
                 return contribution; //no flicker
             }
